7131d2bd588ce6724e15c21506d082f06be71717
Conor O'Donnell <ronocod@users.noreply.github.com>
2017-05-02 10:49:00 +0100

    Simplify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 type go > /dev/null || { echo "Couldn't find go command'" ; exit 1; }
 
+# Print a dot for every second the setup takes
 while :;do printf .;sleep 1;done &
-#Die with parent if we die prematurely
 trap "kill $!" EXIT
 
-echo "\nGetting mux files"
-go get -u github.com/gorilla/mux
-echo "\nGetting gouuid files"
-go get -u github.com/nu7hatch/gouuid
-echo "\nGetting go-bindata files"
-go get -u github.com/jteeuwen/go-bindata/...
-echo "\nGetting gomobile files"
-go get -u golang.org/x/mobile/cmd/gomobile
+echo "\nGetting packages"
+go get -u github.com/gorilla/mux github.com/nu7hatch/gouuid github.com/jteeuwen/go-bindata/... golang.org/x/mobile/cmd/gomobile
 echo "\ngomobile init 📱"
 type gomobile > /dev/null || { echo "Couldn't find gomobile, is \$GOPATH/bin on your \$PATH?" ; exit 1; }
 if [ -n "$ANDROID_NDK" ]; then
@@ -25,5 +19,5 @@ go install github.com/intercom/speakeasy/cmd/speakeasy
 
 echo "\nDone!"
 
-#Kill the loop and unset the trap or else the pid might get reassigned and we might end up killing a completely different process
+# Kill the dot printing process
 kill $! && trap " " EXIT
